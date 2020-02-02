712e80ce361906c595ab881ca0ff58cc53e11250
Sandrine Bédard <sandrinebedard@hotmail.com>
2020-02-02 17:17:03 -0500

    Update calendar.js
    Was not the final version, uploaded the right version

--- public/calendar.js
+++ public/calendar.js
@@ -1,49 +1,82 @@
 'use strict';
 
-var cal = document.getElementById("calendrier");
+// Variables globales
+var nbHeures = "";
+var nbJours = "";
+
+// Unicode du symbole
 var checkMark = "&#10003";
-var symbole = "";
+
+// Variable utilise pour le onMove
+// Si on a cliqué sur une case vide, onMove cherche a mettre des checkmarks
+// sur les autres cases vide, autrement, si on click sur un checkmark,
+// on cherche a enlever les checkmarks avec onMove.
+var symbole = ""; 
 
 document.addEventListener('DOMContentLoaded', function() {
-
-    // TODO: Ajoutez ici du code qui doit s'exécuter au chargement de
-    // la page
+    // Variables fournis dans l'ennonce
+    var cal = document.getElementById("calendrier");
+    nbHeures = cal.dataset.nbheures;
+    nbJours = cal.dataset.nbjours;
 });
 
 function onClick(event) {
-
-    /* La variable t contient l'élément HTML sur lequel le clic a été
-       fait. Notez qu'il ne s'agit pas forcément d'une case <td> du
-       tableau */
+    // La variable t contient l'élément HTML sur lequel le clic a été fait
     var t = event.target;
 
-    // Attribut id de l'élément sur lequel le clic a été fait
+    // Id de l'élément sur lequel le clic a été fait
     var id = t.id;
-    var contenu =  document.getElementById(id).innerHTML;
-    if (id[1] == "-") {
+    // Le nom de la balise qui contient le html
+    var localName = t.localName;
+
+    // S'il s'agit d'une cellule
+    if (localName == "td") {
+        // On regarde ce que contient la cellule
+        var contenu =  document.getElementById(id).innerHTML;
         if (contenu == "") {
+            // Si le contenu est vide, on cherche a mettre des checkMarks
             symbole = checkMark;
         } else {
+            // Autrement on cherche a vider les cellules
             symbole = "";
         }
+        // On change le contenu pour le symbole
+        document.getElementById(id).innerHTML = symbole;
     }
-    document.getElementById(id).innerHTML = symbole;
+
 };
 
 function onMove(event) {
-    // TODO
-
+    // La variable t contient l'élément HTML sur lequel le clic a été fait
     var t = event.target;
-    var id = t.id;
 
+    // Id de l'élément sur lequel le clic a été fait
+    var id = t.id;
+    // Le nom de la balise qui contient le html
+    var localName = t.localName;
+
+    // Si le contenu de la cellule n'est pas le symbole, on y insert le symbole
     var contenu =  document.getElementById(id).innerHTML;
-    if (event.buttons == 1 && id[1] == "-" && contenu != symbole) {
+    if (event.buttons == 1 && localName == "td" && contenu != symbole) {
         document.getElementById(id).innerHTML = symbole;
     }
 };
 
+// Encode en binaire les positions disponibles du participant
 var compacterDisponibilites = function() {
-    // TODO
+    var dispo = "";
 
-    return '0000000';
+    // On regarde tous les cases de disponibilité
+    for (var i = 0; i < nbHeures; i++) {
+        for (var j = 0; j < nbJours; j++) {
+            var id = j + "-" + i;
+            if (document.getElementById(id).innerHTML != "") {
+                dispo += "1"; // 1 si le participant a coche la case
+            } else {
+                dispo += "0"; // 0 si pas disponible
+            }
+        }
+    }
+    
+    return dispo;
 };
